dc24c291ca14c440acb3cdc2bac4ef595bbfc3cd
Regis A. Despres <regis.despres@gmail.com>
2025-11-14 23:31:48 +0100

    feat: Add Qdrant formula for Homebrew

---
+++ Formula/qdrant.rb
@@ -0,0 +1,25 @@
+class Qdrant < Formula
+  desc "qdrant server"
+  arch arm: "aarch64", intel: "x86_64"
+  homepage "https://github.com/qdrant/qdrant"
+  version "1.15.5"
+  on_arm do
+    url "https://github.com/qdrant/qdrant/releases/download/v#{version}/qdrant-aarch64-apple-darwin.tar.gz"
+    sha256 "e712f362e1e9aff18bb062f15c05fa384282ededc7d49f1935792cdc64e25222"
+  end
+  on_and do
+    url "https://github.com/qdrant/qdrant/releases/download/v#{version}/qdrant-amd64-apple-darwin.tar.gz"
+    sha256 "b25f5512f2b696bae84752ff50752a77eec2ce6955e00847816e05c6344d6af9"
+  end
+  
+
+  def install
+    bin.mkpath
+    prefix.install "qdrant"
+    mv "#{prefix}/qdrant", "#{bin}/qdrant"
+  end
+
+  test do
+    system "script", "-q", "/dev/null", bin/"qdrant"
+  end
+end
