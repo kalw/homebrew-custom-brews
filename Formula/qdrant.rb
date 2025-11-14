343271a8abf2de8b0708d8d1e4cb54dcd139e9db
Regis A. Despres <regis.despres@gmail.com>
2025-11-14 23:33:52 +0100

    fix: Change 'on_and' to 'on_intel' in qdrant.rb

--- Formula/qdrant.rb
+++ Formula/qdrant.rb
@@ -7,7 +7,7 @@ class Qdrant < Formula
     url "https://github.com/qdrant/qdrant/releases/download/v#{version}/qdrant-aarch64-apple-darwin.tar.gz"
     sha256 "e712f362e1e9aff18bb062f15c05fa384282ededc7d49f1935792cdc64e25222"
   end
-  on_and do
+  on_intel do
     url "https://github.com/qdrant/qdrant/releases/download/v#{version}/qdrant-amd64-apple-darwin.tar.gz"
     sha256 "b25f5512f2b696bae84752ff50752a77eec2ce6955e00847816e05c6344d6af9"
   end
