f6aced5a9e841cf3c673c39234801dc9c6978a86
Regis A. Despres <regis.despres@gmail.com>
2025-11-15 10:51:45 +0100

    fix: Refactor Qdrant formula for dynamic OS and arch
    Refactor Qdrant formula to set OS and architecture variables dynamically based on CPU type.

--- Formula/qdrant.rb
+++ Formula/qdrant.rb
@@ -1,16 +1,24 @@
 class Qdrant < Formula
   desc "qdrant server"
-  arch arm: "aarch64", intel: "x86_64"
+  @@os="XXX"
+  @@arch="XXX"
+
+  on_macos do
+    @@os="osx"
+    if Hardware::CPU.arm?
+      @@arch="arm64"
+      url "https://github.com/qdrant/qdrant/releases/download/v#{version}/qdrant-aarch64-apple-darwin.tar.gz"
+      sha256 "e712f362e1e9aff18bb062f15c05fa384282ededc7d49f1935792cdc64e25222"
+    end
+
+    if Hardware::CPU.intel?
+      @@arch="x86_64"
+      url "https://github.com/qdrant/qdrant/releases/download/v#{version}/qdrant-amd64-apple-darwin.tar.gz"
+      sha256 "b25f5512f2b696bae84752ff50752a77eec2ce6955e00847816e05c6344d6af9"
+    end
+  end
   homepage "https://github.com/qdrant/qdrant"
   version "1.15.5"
-  on_arm do
-    url "https://github.com/qdrant/qdrant/releases/download/v#{version}/qdrant-aarch64-apple-darwin.tar.gz"
-    sha256 "e712f362e1e9aff18bb062f15c05fa384282ededc7d49f1935792cdc64e25222"
-  end
-  on_intel do
-    url "https://github.com/qdrant/qdrant/releases/download/v#{version}/qdrant-amd64-apple-darwin.tar.gz"
-    sha256 "b25f5512f2b696bae84752ff50752a77eec2ce6955e00847816e05c6344d6af9"
-  end
   
 
   def install
